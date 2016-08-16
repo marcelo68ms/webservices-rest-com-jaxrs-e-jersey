5b0ec7bb25044c69bdfe2b913ecf4ecf17655046
Marcelo Santos <marcelo.santos@senior.com.br>
2016-08-16 17:38:58 -0300

    Codificação do Teste de Produto.

--- src/test/java/br/com/alura/loja/ProdutoTest.java
+++ src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,5 +1,25 @@
 package br.com.alura.loja;
 
-public class ProdutoTest {
+import java.io.IOException;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ProdutoTest {
+	
+	@Test
+	public void testaAcessoURI() throws IOException {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/projetos").request().get(String.class);
+		
+		Assert.assertNotNull(conteudo);
+		
+	}
 }
