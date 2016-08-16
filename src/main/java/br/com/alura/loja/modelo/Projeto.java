f414e6123669f744785aebd657e4a78dd9ca6a3f
Marcelo Santos <marcelo.santos@senior.com.br>
2016-08-16 16:37:18 -0300

    Inclusão das classes relativas a Projeto.

---
+++ src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -0,0 +1,34 @@
+package br.com.alura.loja.modelo;
+
+public class Projeto {
+	private String nome;
+	private long id;
+	private int anoDeInicio;
+	
+	public Projeto(long id, String nome, int anoDeInicio) {
+		super();
+		this.nome = nome;
+		this.id = id;
+		this.anoDeInicio = anoDeInicio;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getAnoDeInicio() {
+		return anoDeInicio;
+	}
+	public void setAnoDeInicio(int anoDeInicio) {
+		this.anoDeInicio = anoDeInicio;
+	}
+	
+}
