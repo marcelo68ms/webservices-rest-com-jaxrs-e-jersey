74e6dce8789f21f28470a2769fb160591a4579e5
Marcelo Santos <marcelo.santos@senior.com.br>
2016-08-17 17:31:35 -0300

    Finalização da Aula #7

--- src/test/java/br/com/alura/loja/ProjetoTest.java
+++ src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -2,10 +2,7 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -15,8 +12,6 @@ import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
 
-import br.com.alura.loja.modelo.Carrinho;
-import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 
 public class ProjetoTest {
@@ -44,21 +39,4 @@ public class ProjetoTest {
 		Assert.assertEquals("Minha loja", projeto.getNome());
 	}
 	
-	@Test
-	public void testarInclusaoDeCarrinho() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		
-		Carrinho carrinho = new Carrinho();
-        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
-        carrinho.setRua("Rua Vergueiro");
-        carrinho.setCidade("Sao Paulo");
-        String xml = carrinho.toXML();
-        
-        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
-
-        Response response = target.path("/carrinhos").request().post(entity);
-        Assert.assertEquals("<status>sucesso</status>",  response.readEntity(String.class));
-	}
-
 }
